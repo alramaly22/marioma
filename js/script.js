d8526b87fed9652fd5062f72484ab28a416f31b7
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2023-03-07 00:18:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,112 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+var typed = new Typed(".auto-type",{
+    strings : [" marioma " ,"Hany" , "Elwakil"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+
+})
+var typed=new Typed(".input",{
+    strings:["Strong Independent Woman"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// var i=0,text;
+// text ="Marioma Hany Elwakil "
+
+// function typing(){
+//     if(i<text.length){
+//         document.getElementById("text").innerHTML +=text.charAt(i);
+//         i++;
+//         setTimeout(typing,50);
+//     }
+// }
+// typing();
+
+
+
+
+
+
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
